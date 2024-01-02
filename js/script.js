00bc4f9443336c84585316d16ba35a3a8e52ab57
MayDamato <maydamato@gmail.com>
2024-01-01 23:53:31 -0300

    nova branche criada para testar formulário por whatsapp

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ closeBtn.onclick = () =>{
 window.onscroll = () =>{
     navbar.classList.remove('active');
 };
+
+function gotowhatsapp(){
+    var name = document.getElementById("nome").value;
+    var phone = document.getElementById("email").value;
+    var email = document.getElementById("assunto").value;
+    var service = document.getElementById("mensagem").value;
+
+    var url = "https://wa.me/5511930491954"
+    + "Name: " + name + "%0a"
+    + "Phone: " + phone + "%0a"
+    + "Email: " + email + "%0a"
+    + "Service" + service;
+
+    window.open(url, '_blank').focus();
+}
